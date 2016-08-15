48db6b1f97752efeecfca8a6038946b0b38860aa
Adão Raul <adao.raul@gmail.com>
2016-08-15 11:33:59 -0300

    scope set from email to identify

--- lib/omniauth/strategies/discord.rb
+++ lib/omniauth/strategies/discord.rb
@@ -3,18 +3,18 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Discord < OmniAuth::Strategies::OAuth2
-      DEFAULT_SCOPE = 'email'
-      
+      DEFAULT_SCOPE = 'identify'
+
       option :name, 'discord'
-      
+
       option :client_options, {
         :site          => 'https://discordapp.com/api',
         :authorize_url => 'oauth2/authorize',
         :token_url     => 'oauth2/token'
       }
-      
+
       option :authorize_options, [:scope]
-      
+
       uid { raw_info['id'] }
 
       info do
@@ -23,8 +23,7 @@ module OmniAuth
           :username      => raw_info['username'],
           :discriminator => raw_info['discriminator'],
           :avatar        => raw_info['avatar'],
-          :verified      => raw_info['verified'],
-          :email         => raw_info['email']
+          :verified      => raw_info['verified']
         }
       end
 
@@ -37,18 +36,18 @@ module OmniAuth
       def raw_info
         @raw_info = access_token.get('users/@me').parsed
       end
-      
+
       def callback_url
         # Discord does not support query parameters
         full_host + script_name + callback_path
       end
-      
+
       def authorize_params
         super.tap do |params|
           options[:authorize_options].each do |option|
             params[option] = request.params[option.to_s]
           end
-          
+
           params[:redirect_uri] = options[:redirect_uri] unless options[:redirect_uri].nil?
           params[:scope] ||= DEFAULT_SCOPE
         end
